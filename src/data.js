04ef4b9b9cc4c41ab389898deae669d74f5e67ec
Maximiliano <maximartins45@gmail.com>
2022-10-15 22:41:34 -0300

    Agregando proyecto al portafolio

--- src/data.js
+++ src/data.js
@@ -1,4 +1,5 @@
 import clima from "../public/images/proyectos/clima.jpg";
+import lista from "../public/images/proyectos/lista.jpg";
 import odontologia from "../public/images/proyectos/fas.jpg";
 import dog from "../public/images/proyectos/pidogs.JPG"
 import sneakers from "../public/images/proyectos/ecommerce.JPG"
@@ -26,7 +27,16 @@ const proyectos = [
     link: "https://github.com/MaxiiMartins/10-app-react/tree/main/clima"
   },
   {
-    id: 3,
+    id:3,
+    title:"Lista de tarea",
+    description:"Es una aplicación web simple donde podés gestionar una lista de tareas pendientes, podés agregar una tarea a tu lista y eliminar las tareas realizadas. Espero tengas una buena experiencia utilizando la aplicación. Saludos 👋",
+    src: lista,
+    url: "/lista",
+    deploy: "https://lista-de-tarea-mx.vercel.app/",
+    link:"https://github.com/MaxiiMartins/app-todo-react"
+  },
+  {
+    id: 4,
     title: "Dogs",
     description:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum earum ad ea! Impedit odio doloremque sequi corporis eligendi beatae atque quos",
@@ -36,7 +46,7 @@ const proyectos = [
     link: "https://github.com/MaxiiMartins/PI-Dogs"
   },
   {
-    id: 4,
+    id: 5,
     title: "Sneakers E-Commerce",
     description:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum earum ad ea! Impedit odio doloremque sequi corporis eligendi beatae atque quos",
@@ -46,7 +56,7 @@ const proyectos = [
     link: "https://github.com/MaxiiMartins/pfinal_client"
   },
   {
-    id: 5,
+    id: 6,
     title: "pelisflix",
     description:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum earum ad ea! Impedit odio doloremque sequi corporis eligendi beatae atque quos",
