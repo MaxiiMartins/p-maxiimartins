7909fbe7dbf30d224f07ded55bc383c84f4c2dc3
Maximiliano <maximartins45@gmail.com>
2022-10-15 21:35:53 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Habilidades from '../components/Habilidades'
+import HomePage from '../components/HomePage'
+import SobreMi from '../components/SobreMi'
+import Proyectos from '../components/Proyectos'
+import Contactos from '../components/Contactos'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Maxi Martins</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <HomePage/>
+      <Proyectos/>
+      <Habilidades/>
+      <SobreMi/>
+      <Contactos/>
+    </div>
+  )
+}
+
