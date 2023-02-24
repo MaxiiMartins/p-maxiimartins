cfb45a3309cb64dc462b06e5b8db6c98a650a14e
MaxiiMartins <maximartins45@gmail.com>
2023-02-23 23:35:27 -0300

    agregando etiqueta meta para el seo

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,12 @@ export default function Home() {
       <Head>
         <title>Maxi Martins</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title"              content="Maxi Martins" />
+        <meta property="description"           content="Desarrollo de Paginas Web | Diseño de Paginas Web en Buenos Aires - Zona Sur | Diseño Pagina Web." />
+        <meta property='keywords'              content='diseño web, diseño de paginas web, paginas web, diseño de pagina web, creacion web, diseñador de paginas web, paginas web en zona norte, paginas web en buenos aires, diseño web en capital federal, diseño web en zona sur, diseño web en buenos aires, armado de paginas web, maxi martins,maxii martins, maximartins' />
+        <meta property="og:description"        content="Sitios Profesionales y de Diseño Elegante. Adaptables 100% a Dispositivos Móviles" />
+	      <meta property="og:image"              content="https://maxiimartins.vercel.app/favicon.ico" />
       </Head>
       <HomePage/>
       <Proyectos/>
